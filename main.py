7c950d7b85a7ecfe9c88d02615d9c710cfbc5219
Matt G <47051509+rat-daddy@users.noreply.github.com>
2021-10-26 22:08:24 -0700

    completed web scraper
    web scraper logic & scraping is done.
    design is... not.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import re
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import csv
+# import tensorflow 
+
 
 
 
@@ -17,74 +19,84 @@ urls = []
 count = 0
 limit = 10
 for line in lines:
-    if count < limit:
-        if line.startswith("<url>"):
-            if "bonappetit.com/recipe/" in line:
-                if "recipe/recipe" not in line:
-                    start = line.index("<loc>") + len("<loc>")
-                    end = line.index("</loc>")
-                    newLine = line[start:end]
-                    titleStart = len("https://www.bonappetit.com/recipe/")
-                    if re.match("[a-zA-Z0-9-]*$", newLine[titleStart:]):
-                        # urls.append(newLine[titleStart:])
-                        urls.append(newLine)
-                        count += 1
+
+    if line.startswith("<url>"):
+        if "bonappetit.com/recipe/" in line:
+            if "recipe/recipe" not in line:
+                start = line.index("<loc>") + len("<loc>")
+                end = line.index("</loc>")
+                newLine = line[start:end]
+                titleStart = len("https://www.bonappetit.com/recipe/")
+                if re.match("[a-zA-Z0-9-]*$", newLine[titleStart:]):
+                    # urls.append(newLine[titleStart:])
+                    urls.append(newLine)
+                    
 urls.sort()
 
-# output = open('bonappetit_data.csv', 'w+', encoding='utf-8', newline='')
-# writer = csv.writer(output, delimiter=',', quotechar='"',quoting=csv.QUOTE_MINIMAL)
-# writer.writerow(['title','url', 'author', 'servings', 'ingredients','instructions'])
+output = open('bonappetit_data.csv', 'w+', encoding='utf-8-sig', newline='')
+writer = csv.writer(output, delimiter=',', quotechar='"',quoting=csv.QUOTE_MINIMAL)
+writer.writerow(['title','url', 'author', 'servings', 'ingredients','steps'])
+
 for url in urls:
-    recipe_page = url
-    page = urlopen(recipe_page)
-    soup = bs(page, 'html.parser')    
-    name_box = soup.find('h1')
-    title = name_box.text.strip()
-    print(title)
-    # print(url)
+    try:
+        recipe_page = url
+        page = urlopen(recipe_page)
+        soup = bs(page, 'html.parser')    
+        name_box = soup.find('h1')
+        if (name_box != None):
+            title = name_box.text.strip()   
+        # print(title)
+        # print(url)
+        
+        author_box = soup.find('span', {'itemprop': 'name'})
+        if (author_box != None):
+            author = author_box.text.strip()
+        
+        # print(author)
+        
+        
+        ingredients = []
+        ingredient_box = soup.find('div',  {'data-testid': 'IngredientList'})
+        if(ingredient_box != None):
+            if (ingredient_box.p != None):
+                servings = ingredient_box.p.text
+            amount = []
+            item = []
+            
+            last_tag = 'div'
+            
+            ingredient_box = ingredient_box.find('div')
+            if ingredient_box != None:
+                ingredient_box = ingredient_box.find_all()
+                if ingredient_box != None:
+                    for tag in ingredient_box:
+                        if tag.name == last_tag:
+                            amount.append("Sub Recipe:")
+                        if tag.name == 'div':
+                            item.append(tag.text)
+                            last_tag = 'div'
+                        if tag.name == 'p':
+                            amount.append(tag.text)
+                            last_tag = 'p'
+            
+            ingredients = zip (amount, item)
+            # print("SERVINGS: ", servings)
+            ingredients = [l for l in ingredients]
+        
     
-    author_box = soup.find('span', {'itemprop': 'name'})
-    author = author_box.text.strip()
-    # print(author)
+        
+        steps = []
+        preparation_box = soup.find('div',  {'data-testid': 'InstructionsWrapper'})
+        if(preparation_box != None):
+            preparation_box = preparation_box.find_all('p')
+            if preparation_box != None:
+                for p in preparation_box:
+                    steps.append(p.text)
+        # print(steps)
     
-    
-    
-    
-    
-    
-    ingredients = []
-    ingredient_box = soup.find('div',  {'data-testid': 'IngredientList'})
-    servings = ingredient_box.p.text
-    amount = []
-    item = []
-    p_tags = ingredient_box.div.find_all('p')
-    for p in p_tags:
-        amount.append(p.text)
-        print(p.text)
-    div_tags = ingredient_box.div.find_all('div')
-    for div in div_tags:
-        item.append(div.text)
-        print(div.text)
-    ingredients = zip (amount, item)
-    print("SERVINGS: ", servings)
-    print(ingredients, "\n")
-    
-    # ingredient_box = soup.find('div',  {'data-testid': 'IngredientList'})
-    # ingredients = ingredient_box.text
-    # ingredients = ingredients[11:]
-    # servings = ""
-    # if "servings" in ingredients[:15].lower():
-    #     s_index = ingredients.lower().index("servings") + 8
-    #     servings = ingredients[:s_index]
-    #     ingredients = ingredients[s_index:]
-    # print("servings ", servings)
-    # print(ingredients, "\n")
-    
-    preparation_box = soup.find('div',  {'data-testid': 'InstructionsWrapper'})
-    preparation = preparation_box.text
-    # print(preparation,  "\n")
-    
-    # writer.writerow([title, url, author, servings, ingredients, preparation])
-    
-# output.close()
+        
+        writer.writerow([title, url, author, servings, list(ingredients), steps])
+    except:
+        continue
+output.close()
  
