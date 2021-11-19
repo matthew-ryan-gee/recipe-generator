b59f4547df2d64c6551e1be76518eeb4f3dcf230
Matt G <47051509+rat-daddy@users.noreply.github.com>
2021-11-19 00:14:47 -0800

    spimi implemented
    issue is that BSBI gives 72 more entries than SPIMI...?

--- pipeline.py
+++ pipeline.py
@@ -5,7 +5,6 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from ast import literal_eval
 import pdpipe as pdp
-import re
 
 
 # writer.writerow([title, url, author, servings, list(ingredients), steps])
@@ -22,37 +21,127 @@ def readData(file):
 def getColumn(dataframe, column):
     return dataframe[column]
 
-def processInvertedIndex(frame,column,invertedIndex):
-    from nltk import pos_tag
-    processedIndex = []
-    for entry in invertedIndex:
-        word = entry[0]
-        tag = pos_tag([word])[0][1]
-        entry.append(tag)
-        # print("\n", term, pos_tag([term]))
-        print(entry)
-    print(len(invertedIndex))
+
+
+# # NOT FUNCTIONAL
+# def processInvertedIndex(frame,column,invertedIndex):
+#     from nltk import pos_tag
+#     processedIndex = []
+#     for entry in invertedIndex:
+#         word = entry[0]
+#         tag = pos_tag([word])[0][1]
+#         entry.append(tag)
+#         # print("\n", term, pos_tag([term]))
+#         print(entry)
+#     print(len(invertedIndex))
     
 
-def createInvertedIndex(frame, column, TDI):
+def BSBI_index(frame, column, TDI):
+    import time
+    start = time.time()
+    
     TDI = sorted(TDI)
-    invertedIndex = []
+    inverted_index = dict()
     for pair in TDI:
         term = pair[0]
         docID = pair[1]
-        if len(invertedIndex) > 0 and pair[0] == invertedIndex[-1][0]:
-        #if invertedIndex is not empty AND the word of thre Row is not equal to the last row entered in the iIndex
-            #increase the frequency of that word
-            invertedIndex[-1][1] = invertedIndex[-1][1] + 1
-            #add the docID of the pair to the posting list
-            invertedIndex[-1][2].append(pair[1])
+        
+        #if invertedIndex is not empty AND the term is the last item in the index
+        if len(inverted_index) > 0 and term in inverted_index:
+            
+            #if last document listed in list of terms is the same as the new one
+            if docID in inverted_index[term]:
+                inverted_index[term][docID] = inverted_index[term][docID] + 1
+                
+            #if last document listed in list of terms is NOT the same as the new one, then add the new document and set value to 1
+            else:
+                inverted_index[term][docID] = 1
+            
+            
+        #if term not in the list, then just make dict[term]'s value a new dict containing its frequency in the document.
         else:
-            postingList = []
-            postingList.append(docID)
-            invertedIndex.append( [term, 1, postingList] )
-    # for row in invertedIndex:
-    #     print("\n", row)
-    processInvertedIndex(frame,column,invertedIndex)
+            inverted_index[term] = dict()
+            inverted_index[term][docID] = 1
+
+    
+    # import pprint 
+    # output_s = pprint.pformat(dict(inverted_index))
+    
+    # with open('BSBI_index.txt','w') as f:
+    #     f.write(output_s)
+    
+    
+    print("BSBI: ", len(inverted_index.keys()))
+    end = time.time()
+    print("BSBI time: ", end - start)
+    
+#     # processInvertedIndex(frame,column,invertedIndex)
+    
+#dict(term, dict(id: freq))
+def SPIMI_index(frame,column,TDI):
+    import time
+    start = time.time()
+    
+    from mergedeep import merge
+    block_list = []
+    new_block = dict()
+    block_size = 500
+    block_index = 0
+    
+    def search_block_list(key, value):
+        for block in block_list:
+            if key in block:
+                if value in block[key]:
+                    block[key][value] = block[key][value] + 1
+                else:
+                    block[key][value] = 1
+                return True
+        else:
+            return False
+    
+    for pair in TDI:
+        term = pair[0] #key
+        docID = pair[1] #value
+        
+        if term in new_block:
+            if docID in new_block[term]:
+                new_block[term][docID] = new_block[term][docID] + 1
+            else:
+                new_block[term][docID] = 1
+        else:
+            #if not in block list, do the following (other upates are in the function)
+            if search_block_list(term, docID) == False:
+                if len(new_block) <= block_size:
+                    new_block[term] = dict()
+                    new_block[term][docID] = 1
+                    
+                else:
+                    block_list.append(new_block)
+                    block_index = block_index + 1
+                    new_block = dict()
+                    # print("new block", block_index, "!")
+    
+    inverted_index = dict()
+    
+    print("BLOCK LIST: ", len(block_list))
+    for block in block_list:
+        merge(inverted_index, block)
+        
+
+    # import pprint 
+    # output_s = pprint.pformat(dict(inverted_index))
+    # with open('SPIMI_index.txt','w') as f:
+    #     f.write(output_s)
+        
+    
+    print("SPIMI: ", len(inverted_index.keys()))
+    end = time.time()
+    print("SPIMI time: ", end - start)
+    
+    
+    # processIndex(frame, column, inverted_index)
+    
+    
 def createTDI(frame, column):
     docID = 0
     TDI = []
@@ -60,26 +149,32 @@ def createTDI(frame, column):
         for word in row:
             TDI.append( (word, docID) )
         docID = docID + 1
-        # print("\n ", docID)
-    
-    
-    # print("createTDI ")
-    
-    createInvertedIndex(frame,column,TDI)
-    # for row in TDI:
-    #     print("\n", row)
 
-def sortColumn(frame, column):
-    sortedColumn = []
     
-    for row in frame[column]:
-        row = sorted(set(row))
-        sortedColumn.append(row)
+    BSBI_index(frame,column,TDI)
+    SPIMI_index(frame,column, TDI)
+    
+    
+
+
+
+# #IS THIS EVEN NECESSARY?
+# def sortColumn(frame, column):
+#     sortedColumn = []
+    
+#     for row in frame[column]:
+#         row = sorted(set(row))
+#         sortedColumn.append(row)
    
-    frame[column] = sortedColumn
-    # print("sortedColumn")
-    # print(frame)
-    createTDI(frame, column)
+#     frame[column] = sortedColumn
+#     # print("sortedColumn")
+#     # print(frame)
+#     createTDI(frame, column)
+    
+    
+    
+    
+    
 
 def removeStopWords(frame, column):
     from nltk.corpus import stopwords
@@ -98,7 +193,8 @@ def removeStopWords(frame, column):
     # print("removeStopWords")
     # print(frame[column])
     
-    sortColumn(frame, column)
+    # sortColumn(frame, column)
+    createTDI(frame,column)
     
 def porterStemmer(frame, column):
     stemmer = PorterStemmer()
@@ -163,12 +259,21 @@ def ingredient_pipeline(frame):
     
     tokenize(frame, 'ingredientsCleaned')
     
-def title_pipeline(column):
+def title_pipeline(frame):
     print("not implemented")
+    
+    # for row in frame:
+    #     tokenize(frame,'title')
+    
+def steps_pipeline(frame):
+    print("not implemented")
+    # for row in frame:
+    #     tokenize(frame,'title')
     
 def pipelineStart(dataframe):
     ingredient_pipeline(dataframe)
     title_pipeline(dataframe)
+    steps_pipeline(dataframe)
 
 
 
